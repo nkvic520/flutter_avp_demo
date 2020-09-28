ada75345bd812a9db971f8ef18d128519aed69a2
sunny <nkvic520@hotmail.com>
2020-09-28 17:01:32 +0800

    '20200928'

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutteravpdemo/video_player.dart';
+import 'package:flutter_avp_demo/video_player.dart';
 
 void main() => runApp(MyApp());
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutteravpdemo/main.dart';
+import 'package:flutter_avp_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
